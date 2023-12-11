b96631c1bc677a87c510213398929365ece7a446
tanvirfsd <tahasanpro@gmail.com>
2023-12-11 19:44:22 +0600

    create starter template

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home/Home";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
