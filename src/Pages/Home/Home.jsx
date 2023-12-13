c745f9acf794441598c56287a7d1b80e1d72f3b4
tanvirfsd <tahasanpro@gmail.com>
2023-12-13 10:33:51 +0600

    add picture

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <section className="container mx-auto">
-        <div className="hero-content flex flex-col md:flex-row justify-between items-center ">
+        <div className="hero-content flex flex-col-reverse md:flex-row justify-between items-center ">
           <div className="">
             <h3 className="font-medium tracking-wider ">WELCOME TO MY WORLD</h3>
             <h1 className="text-4xl lg:text-5xl font-bold">
@@ -56,7 +56,9 @@ const Home = () => {
             </div>
           </div>
           <div className="right">
-            <div className="right_img bg-gradient-to-br from-gray-200 to-white shadow-md rounded-xl h-96 w-80 md:w-96 "></div>
+            <div className="right_img bg-gradient-to-br rounded-full from-gray-200 to-white shadow-md h-80  sm:h-96 w-80 sm:w-96 ">
+              <img src="https://i.ibb.co/8Y6rYp4/profile.png" className="rounded-full object-cover h-full w-full" alt="" />
+            </div>
           </div>
         </div>
       </section>
